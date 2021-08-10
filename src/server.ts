a55be87464e5dba0e96a07b7749236463e5afc4b
mbantwerpes <marcel.blesing@antwerpes.com>
2021-08-10 16:26:02 +0200

    Rename req and res variables

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,17 @@
+import express from 'express';
 import { readCredentials } from './utils/credentials';
 
-(async () => {
-  console.log(await readCredentials());
-})();
+const app = express();
+const port = 3000;
+
+app.get('/', (_request, response) => {
+  response.send('Hello World!');
+});
+
+app.get('/credentials', async (_request, response) => {
+  response.send(await readCredentials());
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
