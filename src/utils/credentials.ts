127d350e4b1d6bb9619ade33809c3cc5f5f66751
mbantwerpes <marcel.blesing@antwerpes.com>
2021-08-10 14:52:44 +0200

    Remove unnecessary types

--- src/utils/credentials.ts
+++ src/utils/credentials.ts
@@ -2,8 +2,8 @@ import { readFile } from 'fs/promises';
 import { DB, Credential } from '../types';
 
 export const readCredentials = async (): Promise<Credential[]> => {
-  const response: string = await readFile('src/db.json', 'utf-8');
+  const response = await readFile('src/db.json', 'utf-8');
   const db: DB = JSON.parse(response);
-  const credentials: Credential[] = db.credentials;
+  const credentials = db.credentials;
   return credentials;
 };
