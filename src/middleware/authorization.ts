99c40d1967a29466b508a3dfcfa3631f1888098d
mbantwerpes <marcel.blesing@antwerpes.com>
2021-08-12 17:48:37 +0200

    Get masterPassword from authorizaton header

--- src/middleware/authorization.ts
+++ src/middleware/authorization.ts
@@ -1,7 +1,12 @@
 import { validateMasterPassword } from '../utils/validation';
 
-export const isAuthorized = async (req: any, res: any, next: any) => {
-  if (await validateMasterPassword('123')) {
+export const isAuthorized = async (
+  req: any,
+  res: any,
+  next: any
+): Promise<void> => {
+  const masterPassword = req.headers.authorization;
+  if (await validateMasterPassword(masterPassword)) {
     next();
   } else {
     res.send(401, 'Unauthorized');
