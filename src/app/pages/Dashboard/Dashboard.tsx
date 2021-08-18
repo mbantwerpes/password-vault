8462855455ec7350063cb8ab656cf93fe8cdc2b9
mbantwerpes <marcel.blesing@antwerpes.com>
2021-08-18 17:19:24 +0200

    Add Header and search input

--- src/app/pages/Dashboard/Dashboard.tsx
+++ src/app/pages/Dashboard/Dashboard.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import styles from './Dashboard.module.css';
 
 const Dashboard = (): JSX.Element => {
-  return <div className={styles.placeholder}>Dashboard</div>;
+  return (
+    <div className={styles.container}>
+      <h1>Dashboard</h1>
+      <p>This is my password vault</p>
+      <input type="text" placeholder="Search..." />
+    </div>
+  );
 };
 
 export default Dashboard;
