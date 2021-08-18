9d3db8c85745b392c343e001e97fbc8756001f9f
mbantwerpes <marcel.blesing@antwerpes.com>
2021-08-18 16:49:44 +0200

    Setup basic component and remove boilerplate code

---
+++ src/app/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Dashboard.module.css';
+
+const Dashboard = (): JSX.Element => {
+  return <div className={styles.placeholder}>Dashboard</div>;
+};
+
+export default Dashboard;
