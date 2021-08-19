1a0048c278a8f9d8b583bd295deaa9c37585425d
mbantwerpes <marcel.blesing@antwerpes.com>
2021-08-19 10:32:13 +0200

    Add route for dashboard

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 // import styles from './App.module.css';
 import Dashboard from './pages/Dashboard/Dashboard';
 
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App(): JSX.Element {
   return (
     <Router>
-      <Dashboard />
+      <Switch>
+        <Route path="/">
+          <Dashboard />
+        </Route>
+      </Switch>
       <div id="modal-root" />
     </Router>
   );
