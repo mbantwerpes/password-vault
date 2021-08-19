f6079871d2041f9bd8451214571f049fcdd5c41d
mbantwerpes <marcel.blesing@antwerpes.com>
2021-08-19 09:39:18 +0200

    Add modal with custom hook to control it

---
+++ src/app/components/Modal/Modal.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import styles from './Modal.module.css';
+
+type Props = {
+  children: React.ReactChild;
+  closeModal: () => void;
+};
+
+const Modal = React.memo(({ children, closeModal }: Props) => {
+  const domEl = document.querySelector('#modal-root');
+
+  if (!domEl) return null;
+
+  // This is where the magic happens -> our modal div will be rendered into our 'modal-root' div, no matter where we
+  // use this component inside our React tree
+  return ReactDOM.createPortal(
+    <div className={styles.container}>
+      <button onClick={closeModal}>Close</button>
+      {children}
+    </div>,
+    domEl
+  );
+});
+
+export default Modal;
---
+++ src/app/hooks/useModal.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+import Modal from '../components/Modal/Modal';
+
+// Renders a modal to the modal root and handles the visibility state
+// of this modal.
+//
+// NOTE: Each modal you want to render should use a separate hook!!! (Not a new useModal hook-file just a separate useModal hook in the component)
+// Otherwise your modals will share their visibility state which might lead
+// to overlapping and unclosable elements.
+export const useModal = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const show = () => setIsVisible(true);
+  const hide = () => setIsVisible(false);
+
+  const RenderModal = ({ children }: { children: React.ReactChild }) => (
+    <>{isVisible && <Modal closeModal={hide}>{children}</Modal>}</>
+  );
+
+  return {
+    show,
+    hide,
+    RenderModal,
+  };
+};
