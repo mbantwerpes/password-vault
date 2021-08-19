5d1e99a551fd5a285201221057d65ac774e8e524
mbantwerpes <marcel.blesing@antwerpes.com>
2021-08-19 10:38:51 +0200

    Add simple navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 // import styles from './App.module.css';
 import Dashboard from './pages/Dashboard/Dashboard';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App(): JSX.Element {
   return (
     <Router>
+      <nav>
+        <Link to="/">Dashboard</Link>
+        <Link to="/services">Services</Link>
+      </nav>
+
       <Switch>
         <Route path="/services">
           <div>Services</div>
