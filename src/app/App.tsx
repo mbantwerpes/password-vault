2884a50fb8eaa00d55dad3e5b33457885a1fb07d
mbantwerpes <marcel.blesing@antwerpes.com>
2021-08-19 10:30:10 +0200

    Add router to App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 // import styles from './App.module.css';
 import Dashboard from './pages/Dashboard/Dashboard';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 function App(): JSX.Element {
   return (
-    <div>
+    <Router>
       <Dashboard />
       <div id="modal-root" />
-    </div>
+    </Router>
   );
 }
 
