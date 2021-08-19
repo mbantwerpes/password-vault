6c3198338a0cd78af64f338eff8efd43f5d59471
mbantwerpes <marcel.blesing@antwerpes.com>
2021-08-19 09:57:13 +0200

    Move modal example to dashboard

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,20 +2,10 @@ import React from 'react';
 // import styles from './App.module.css';
 import Dashboard from './pages/Dashboard/Dashboard';
 
-import { useModal } from './hooks/useModal';
-
 function App(): JSX.Element {
-  const { show, RenderModal } = useModal();
-
   return (
     <div>
       <Dashboard />
-      <div>
-        <button onClick={show}>MODAL anzeigen!</button>
-        <RenderModal>
-          <p>Hello this is your amazing modal</p>
-        </RenderModal>
-      </div>
       <div id="modal-root" />
     </div>
   );
--- src/app/pages/Dashboard/Dashboard.tsx
+++ src/app/pages/Dashboard/Dashboard.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import styles from './Dashboard.module.css';
 
+import { useModal } from '../../hooks/useModal';
+
 const Dashboard = (): JSX.Element => {
+  const { show, RenderModal } = useModal();
+
   return (
     <div className={styles.container}>
       <h1>Dashboard</h1>
       <p>This is my password vault</p>
       <input type="text" placeholder="Search..." />
-      <div>
+      <div onClick={show}>
         <p>Reddit</p>
         <p>max@mustermann.de</p>
       </div>
+      <RenderModal>
+        <p>Hello this is your amazing modal</p>
+      </RenderModal>
     </div>
   );
 };
