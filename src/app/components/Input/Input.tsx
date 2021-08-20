9089d8d083b7c377a11bf2ce21e5c88af9a1b154
mbantwerpes <marcel.blesing@antwerpes.com>
2021-08-20 11:31:44 +0200

    Add value to inputs

--- src/app/components/Input/Input.tsx
+++ src/app/components/Input/Input.tsx
@@ -3,10 +3,11 @@ import React from 'react';
 type InputProps = {
   type: string;
   placeholder: string;
+  value: string;
 };
 
-const Input = ({ type, placeholder }: InputProps): JSX.Element => {
-  return <input type={type} placeholder={placeholder} />;
+const Input = ({ type, placeholder, value }: InputProps): JSX.Element => {
+  return <input type={type} placeholder={placeholder} value={value} />;
 };
 
 export default Input;
